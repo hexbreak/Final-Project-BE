d4b9edc499197fa777b18980a3d1764198c1c1bf
hexbreak <theownerproject@gmail.com>
2021-02-17 03:56:28 +0000

    updates

--- src/models.py
+++ src/models.py
@@ -29,3 +29,22 @@ class User(db.Model):
             "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
+
+# class NowPlaying(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     game_id = db.Column(db.String(250), unique=True, nullable=False)
+#     game_name = db.Column(db.String(250), unique=True, nullable=False)
+#     comment = db.Column(db.String(250), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<NowPlaying %r>' % self.user_id
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "user_id": self.user_id,
+#             "game_id": self.game_id,
+#             "game_name": self.game_name,
+#             "comment": self.comment
+#         }
